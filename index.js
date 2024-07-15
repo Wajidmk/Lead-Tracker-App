ce2277d0bab1faed584092836d2c8247f8f6a61e
Abdul Waajid Ahmed <106549884+Wajidmk@users.noreply.github.com>
2024-07-15 18:00:41 +0530

    D

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ inputBtn.addEventListener("click",function(){
 
 // Log out the items in the myLeads array using a for loop 
 for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
+   ulEl.innerHTML += "<li>" + myLeads[i] +"</li>"
 }
