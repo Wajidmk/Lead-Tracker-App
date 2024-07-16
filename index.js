e2c3043c7604c7ffb84d931aef219e92fc0f8291
Abdul Waajid Ahmed <106549884+Wajidmk@users.noreply.github.com>
2024-07-16 07:05:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    // 2. Call the renderLeads() function
+    inputEl.value = ""
     renderLeads()
 })
 
-// 1. Wrap the code below in a renderLeads() function
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        console.log(listItems)
     }
     ulEl.innerHTML = listItems  
 }
-
